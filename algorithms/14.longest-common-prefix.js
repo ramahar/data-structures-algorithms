9f06737052fbd03fca1869908d130cf42518b1cc
Rayyan <rmahar@ucsd.edu>
2019-04-07 12:39:24 -0700

    Longest common prefix

--- algorithms/14.longest-common-prefix.js
+++ algorithms/14.longest-common-prefix.js
@@ -52,3 +52,29 @@ var longestCommonPrefix = function(strs) {
       return prev.slice(0, i);
     })
 };
+
+// FASTER solution 
+var longestCommonPrefix = function(arr) {
+  if(!arr.length) return ''
+  if(arr.length == 1) return arr[0]
+      
+  var longestStr = arr[0];
+  var length = longestStr.length;
+  
+  while(length > 0) {
+      for(var i = 1; i < arr.length; i++){
+          if (arr[i].substr(0, length) != longestStr) {
+              break
+          } else {
+              if(i == arr.length - 1) {
+                  return longestStr.substr(0, length);
+              }
+          }
+      }
+      
+      length--
+      longestStr = longestStr.substr(0, length)
+  }
+  
+  return '';
+}
