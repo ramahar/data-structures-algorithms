6ae7fa1ee6526c923ccb0e9256b9dade15f67c19
Rayyan <rmahar@ucsd.edu>
2019-04-08 13:56:04 -0700

    Add linked list methods

--- data_structures/linked-list/LinkedList.js
+++ data_structures/linked-list/LinkedList.js
@@ -7,4 +7,36 @@ export default class LinkedList {
     this.tail = null; 
     this.compare = new Comparator(comparatorFunction);
   }
+
+  prepend(value) {
+    //Create new node and make it the head 
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;  
+
+    //If there is no tail, make new node the tail
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    //If there is no head, make new node the head 
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    //Attach new node to the end 
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
+  }
+
+  
+
 }
