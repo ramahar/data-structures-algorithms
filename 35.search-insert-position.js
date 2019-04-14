5979e7f2d9bd25a3f725cb9c2b8a9191136de91a
Rayyan <rmahar@ucsd.edu>
2019-04-14 15:04:59 -0700

    Search insert position

--- 35.search-insert-position.js
+++ 35.search-insert-position.js
@@ -52,6 +52,21 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    
+    if (!nums) return 0;
+   return binarySearch(nums, target, 0, nums.length-1);
 };
 
+var binarySearch = function(array, target, start, end) {
+  
+  if (start > end) return start;
+    
+  const midPoint = Math.floor((start + end)/2);
+  
+// found target
+  if (array[midPoint] === target) return midPoint;
+  
+// search the left side
+  if (array[midPoint] > target) return binarySearch(array, target, start, midPoint - 1);
+  // search the right side
+  if (array[midPoint] < target) return binarySearch(array, target, midPoint + 1, end);
+}
