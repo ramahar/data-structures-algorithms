a4aecece59f475adc774cb786601261a886fc39e
Rayyan <rmahar@ucsd.edu>
2019-04-14 16:17:41 -0700

    First missing positive

---
+++ algorithms/41.first-missing-positive.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=41 lang=javascript
+ *
+ * [41] First Missing Positive
+ *
+ * https://leetcode.com/problems/first-missing-positive/description/
+ *
+ * algorithms
+ * Hard (28.45%)
+ * Total Accepted:    202.8K
+ * Total Submissions: 710.1K
+ * Testcase Example:  '[1,2,0]'
+ *
+ * Given an unsorted integer array, find the smallest missing positive
+ * integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,2,0]
+ * Output: 3
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [3,4,-1,1]
+ * Output: 2
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [7,8,9,11,12]
+ * Output: 1
+ * 
+ * 
+ * Note:
+ * 
+ * Your algorithm should run in O(n) time and uses constant extra space.
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let max = 0;
+    //Object to store numbers seen 
+    let result = {};
+    for (let i = 0; i < nums.length; i++) {
+      let curr = nums[i];
+      result[curr] = curr;
+      max = curr > max ? curr : max 
+    }
+
+    
+};
