9c7092600d1b6cc25199d3eb397c9e2bcb7f72fe
Rayyan <rmahar@ucsd.edu>
2019-04-16 15:20:43 -0700

    myPow

--- algorithms/50.pow-x-n.js
+++ algorithms/50.pow-x-n.js
@@ -50,12 +50,14 @@
  */
 var myPow = function(x, n) {
     //Base case
-    if (n === 0) return 1;
-    if (n === 1) return x;
+    // if (n == 0) return 1;
+    // if (n === 1) return x;
 
-    if (n < 0) {
-      n = -n;
-      x = 1/x;
-    }
-    return (n%2 === 0) ? myPow(x*x, n/2) : x * myPow(x*x, n/2);
+    // if (n < 0) {
+    //   n = -n;
+    //   x = 1/x;
+    // }
+    // return (n%2 === 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+
+    return x**n;
 };
