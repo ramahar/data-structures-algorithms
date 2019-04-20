365b1f6a9ebef4f4ed7541776a5ba23c459cd9af
Rayyan <rmahar@ucsd.edu>
2019-04-20 04:14:31 -0700

    Reverse linked list

--- algorithms/google/206.reverse-linked-list.js
+++ algorithms/google/206.reverse-linked-list.js
@@ -38,8 +38,16 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    var temp = null;
-    var newHead = null;
-
-    
+  return head ? reverse(head, null) : head;
 };
+
+function reverse (node, previous) {
+  let newHead = node;
+
+  // recursive call to tail
+  if (node.next) newHead = reverse(node.next, node);
+  // reverse from tail all the way up to head
+  node.next = previous;
+  
+  return newHead;
+}
