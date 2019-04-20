57649570fe10471e6c4cf913ea5b5d6b7884ecb6
Rayyan <rmahar@ucsd.edu>
2019-04-20 04:12:05 -0700

    Reverse linked list

---
+++ algorithms/google/206.reverse-linked-list.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (53.44%)
+ * Total Accepted:    558.8K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Reverse a singly linked list.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * 
+ * 
+ * Follow up:
+ * 
+ * A linked list can be reversed either iteratively or recursively. Could you
+ * implement both?
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    var temp = null;
+    var newHead = null;
+
+    
+};
