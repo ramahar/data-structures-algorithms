1f7a982ae4fc88f164dca69a013a4b3b525f19b1
Rayyan <rmahar@ucsd.edu>
2019-04-28 00:02:40 -0700

    Queue via stack

--- CTCI/stacks-queues/queue_via_stack.js
+++ CTCI/stacks-queues/queue_via_stack.js
@@ -9,7 +9,14 @@ class MyQueue {
     this.stack1.push(val);
   }
 
+  // Push elements from stack1 to stack2, then pop from stack2
   dequeue() {
-    
+    if (this.stack2.length === 0) {
+      while (this.stack1.length) {
+        let el = this.stack1.pop();
+        this.stack2.push(el);
+      }
+    }
+    return this.stack2.pop();
   }
 }
