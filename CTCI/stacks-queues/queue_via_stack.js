5aafd4c624dc82d37678c30e989143563b321270
Rayyan <rmahar@ucsd.edu>
2019-04-27 23:54:54 -0700

    Stacks and queues

---
+++ CTCI/stacks-queues/queue_via_stack.js
@@ -0,0 +1,15 @@
+// Implement a MyQueue class which implements a queue using two stacks
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(val) {
+    this.stack1.push(val);
+  }
+
+  dequeue() {
+    
+  }
+}
