4f1438cc61d5f11d0c70f53db931f5c220062bcf
Rayyan <rmahar@ucsd.edu>
2019-05-01 00:26:24 -0700

    Subtree of another tree

--- 572.subtree-of-another-tree.js
+++ 572.subtree-of-another-tree.js
@@ -22,12 +22,12 @@
  * @return {boolean}
  */
 var isSubtree = function(s, t) {
-    if (!s) return !t;
-    return isEqual(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+  if (!s) return !t;
+  return isEqual(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
 };
 
 function isEqual(root1, root2) {
-    if (!root1 || !root2) return !root1 && !root2;
-    if (root1.val !== root2.val) return false;
-    return isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
+  if (!root1 || !root2) return !root1 && !root2;
+  if (root1.val !== root2.val) return false;
+  return isEqual(root1.left, root2.left) && isEqual(root1.right, root2.right);
 }
