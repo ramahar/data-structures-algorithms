74097af3889a8c22d927224dfb5b8536cf4ea5cf
Rayyan <rmahar@ucsd.edu>
2019-05-08 21:51:09 -0700

    Valid word square

---
+++ 422.valid-word-square.js
@@ -0,0 +1,18 @@
+/*
+Given a sequence of words, check whether it forms a valid word square.
+A sequence of words forms a valid word square
+if the kth row and column read the exact same string, where 0≤ k < max(numRows, numColumns).
+Note:
+The number of words given is at least 1 and does not exceed 500.
+Word length will be at least 1 and does not exceed 500.
+Each word contains only lowercase English alphabet a - z.
+Example 1:
+  Input: [
+    "abcd",
+    "bnrt",
+    "crmy",
+    "dtye"
+  ]
+Output:
+  true
+*/
