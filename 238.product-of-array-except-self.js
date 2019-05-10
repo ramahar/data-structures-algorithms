d3ae643b056660d8091d91d894e3f49ed64b442f
Rayyan <rmahar@ucsd.edu>
2019-05-09 17:56:00 -0700

    Product except self

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -2,7 +2,9 @@
  * @lc app=leetcode id=238 lang=javascript
  *
  * [238] Product of Array Except Self
- * // Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+ * Given an array nums of n integers where n > 1,  return an array 
+ * output such that output[i] is equal to the product of all the 
+ * elements of nums except nums[i].
 Input:  [1,2,3,4]
 Output: [24,12,8,6]
 Note: Please solve it without division and in O(n).
@@ -12,23 +14,18 @@ Note: Please solve it without division and in O(n).
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    
+    let result = [1];
+
+    for (let i = 1; i < nums.length; i++) {
+      result[i] = result[i-1] * nums[i-1];
+    }
+    // Number to multiply result array with. Updates every iteration
+    let right = 1;
+    for (let i = nums.length-1; i >= 0; i--) {
+      result[i] *= right;
+      right *= nums[i];
+    }
+    return result;
 };
 
-function generate_phrases(phrases) {
-  let result = '';
-  for (let i = 0; i < phrases.length; i++) {
-    let phrase = phrases[i].split(' ');
-    let last = phrase[phrase.length-1];
-    for (let j = i + 1; j < phrases.length-1; j++) {
-      let second = phrases[j].split(' ')
-      if (second[1] === last) {
-        result = phrase.concat(second);
-      }
-    }
-  }
-  return result;
-}
-
-
-console.log(generate_phrases(['writing code', 'code rocks']));
+// productExceptSelf([1,2,3,4]);
