b7ad1b07a11f0e25695005047e0937789d455971
Rayyan <rmahar@ucsd.edu>
2019-05-10 16:38:36 -0700

    Add practice questions

--- practice/recursion.js
+++ practice/recursion.js
@@ -57,3 +57,30 @@ function reverseString(str) {
   let remaining = str.slice(0, str.length-1);
   return str[str.length-1] + reverseString(remaining);
 }
+
+// Write a function, pow(base, exponent), that takes in two numbers.
+// The function should calculate the base raised to the exponent power.
+//
+// Note: 
+// A negative exponent can be calculate by taking the reciprocal of the positive exponent.
+// That is, pow(2, -5) is equal to 1 / pow(2, 5)
+// 
+// Solve this recursively!
+//
+// Examples:
+//
+// pow(2, 0)    // => 1
+// pow(2, 1)    // => 2
+// pow(2, 5)    // => 32
+// pow(3, 4)    // => 81
+// pow(2, -5)   // => 0.03125
+function pow(base, exponent) {
+  if (exponent === 0) return 1;
+  if (exponent === 1) return base;
+
+  if (exponent > 0) {
+    return base * pow(base, exponent-1);
+  } else {
+    return 1 / base * pow(base, exponent+1);
+  }
+}
