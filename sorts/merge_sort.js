c82e7df14335a2eca2dcafe08f5eed8eb4d87ca1
Rayyan <rmahar@ucsd.edu>
2019-05-10 15:26:57 -0700

    Merge sort

---
+++ sorts/merge_sort.js
@@ -0,0 +1,20 @@
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+  let mid = Math.floor(array.length/2);
+  let left = array.slice(0, mid);
+  let right = array.slice(mid+1);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  while (left.length > 0 && right.length > 0) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift);
+    }
+  }
+  return result.concat(left, right);
+}
