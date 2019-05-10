622360dffd8dcf44784e4f62597843330a393b03
Rayyan <rmahar@ucsd.edu>
2019-05-10 15:19:01 -0700

    kth largest element

--- 215.kth-largest-element-in-an-array.js
+++ 215.kth-largest-element-in-an-array.js
@@ -15,3 +15,35 @@ var findKthLargest = function(nums, k) {
 };
 
 // Alternate solution, using quicksort on the side where the number will be 
+var findKthLargest1 = function(nums, k) {
+  k -= 1
+  return findKthLargestImpl(nums, k);
+};
+
+function findKthLargestImpl(nums, k, low = 0, high = nums.length - 1) {
+  const pivot = nums[high];
+  let insertI = low;
+  for (let i = low; i < high; i += 1) {
+      if (nums[i] < pivot) {
+          swap(nums, i, insertI++);
+      }
+  }
+  swap(nums, insertI, high); // Pivot swap
+  
+  if (high - k > insertI) {
+      // Number is on the right side
+      return findKthLargestImpl(nums, k, insertI + 1, high);
+  } else if (high - k === insertI) {
+      // Pivot is the number
+      return pivot;
+  } else {
+      // Number is on the left side
+      return findKthLargestImpl(nums, k - (high - insertI) - 1, low, insertI - 1);
+  }
+}
+
+function swap(arr, i, j) {
+  const tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+}
