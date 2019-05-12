772a11207ea8cc4fe9de25f9804edc4f003c37a3
Rayyan <rmahar@ucsd.edu>
2019-05-11 23:14:26 -0700

    First unique character

--- 387.first-unique-character-in-a-string.js
+++ 387.first-unique-character-in-a-string.js
@@ -18,3 +18,21 @@ var firstUniqChar = function(s) {
 };
 
 // Alternate with Hash map 
+var firstUniqChar = function(s) {
+  let map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      // Duplicates
+      map.set(s[i], 2);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i]) && map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+}
