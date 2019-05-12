f38f0244c0b38bb844ddb8a1fb328d566b1e5f5d
Rayyan <rmahar@ucsd.edu>
2019-05-11 21:36:25 -0700

    Hamming distance

---
+++ 461.hamming-distance.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=461 lang=javascript
+ *
+ * [461] Hamming Distance
+ * 
+ */
+// The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+//
+// Given two integers x and y, calculate the Hamming distance.
+//
+// Note:
+// 0 ≤ x, y < 231.
+//
+// Example:
+//
+// Input: x = 1, y = 4
+//
+// Output: 2
+//
+// Explanation:
+// 1   (0 0 0 1)
+// 4   (0 1 0 0)
+//        ↑   ↑
+//
+// The above arrows point to positions where the corresponding bits are different.
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    // toString(2) converts number to binary
+    return (x ^ y).toString(2).split('').filter(num => num === "1").length;
+
+    //Alternate
+    // return (x ^ y).toString(2).split('0').join('').length;
+};
