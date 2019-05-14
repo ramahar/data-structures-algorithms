e09978f9310ad1edfc951f453480b4cf0ce36e77
Rayyan <rmahar@ucsd.edu>
2019-05-13 17:26:30 -0700

    Add practice problems

---
+++ leetcode/all_problems/problems.js
@@ -0,0 +1,15 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// Example: 
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1]
+function twoSum(nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (map[diff] !== undefined) {
+      return [map[diff], i];
+    }
+    map[nums[i]] = i; 
+  }
+}
+console.log(twoSum([2, 7, 11, 15], 9));
