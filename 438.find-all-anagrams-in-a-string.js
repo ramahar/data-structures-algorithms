627891fa00b7c17483acaece48c0967c6d1389f5
Rayyan <rmahar@ucsd.edu>
2019-05-20 23:43:42 -0700

    Find all anagrams in a string

---
+++ 438.find-all-anagrams-in-a-string.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=438 lang=javascript
+ *
+ * [438] Find All Anagrams in a String
+ * Given a string s and a non-empty string p, 
+ * find all the start indices of p's anagrams in s.
+ * Input:
+ * s: "cbaebabacd" p: "abc"
+ * Output: [0, 6]
+ */
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    
+};
