4f2328efcde8a2441078b6ca0931d48b8c2d4617
Rayyan <rmahar@ucsd.edu>
2019-05-20 23:47:44 -0700

    Find all anagrams in a string

--- 438.find-all-anagrams-in-a-string.js
+++ 438.find-all-anagrams-in-a-string.js
@@ -13,6 +13,27 @@
  * @param {string} p
  * @return {number[]}
  */
+// Sliding window 
 var findAnagrams = function(s, p) {
-    
+    let result = [];
+    let pLength = p.length;
+    let sLength = s.length;
+
+    // Create 2 empty arrays filled with 0's
+    let pWindow = new Array(26).fill(0);
+    let sWindow = new Array(26).fill(0);
+
+    // charCodeAt returns a--> 97, b --> 98, c--> 99, etc
+    [...p].forEach(char => {
+      pWindow[char.charCodeAt(0) - 97]++;
+    });
+
+    [...s].forEach((char, index) => {
+      // jump into next position, and minus the previous chart from window
+      if (index >= pLength) sWindow[s.charCodeAt(index-pLength)-97]--;
+      sWindow[char.charCodeAt(0)-97]++
+      // compare two strings
+      if (pWindow.join()===sWindow.join()) result.push(index+1-pLength);
+    });
+    return result;
 };
