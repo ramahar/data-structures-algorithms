759fbb407fefbb13fb5344d19e8099baff8cb4aa
Rayyan <rmahar@ucsd.edu>
2019-05-25 11:01:08 -0700

    Climbing stairs

--- 70.climbing-stairs.js
+++ 70.climbing-stairs.js
@@ -25,10 +25,41 @@ const getSteps = (n, memo) => {
 };
 
 // Iterative
-  
+const climbStairs1 = function(n) {
+  if (n <= 3) return n;
+  let arr = [1, 2, 3];
+
+  for (let i = 3; i < n; i++) {
+    arr.push(arr[i-1] + arr[i-2]);
+  }
+  // Last element in array will be number of ways to climb n stairs
+  return arr.pop();
+}  
+
+// DYNAMIC Programming
+const climbStairs2 = function(n) {
+  if (n < 0) return 0;
+
+  let steps = new Array(n+1).fill(0); // Array that holds all possible ways to get to a step
+
+  steps[0] = 0; 
+  steps[1] = 1;
+  steps[2] = 2;
+
+  if (n <= 2) {
+    return steps[n];
+  }
+
+  // Calculate every next step based on 2 previous steps
+  for (let i = 3; i <= n; i++) {
+    steps[i] = steps[i-1] + steps[i-2];
+  }
+
+  return steps[n];
+}
 
 // Basic Recursive. SLOW!
-var climbStairs4 = function(n) {
+var climbStairs3 = function(n) {
     if (n <= 3) return n;
     return climbStairs1(n-1) + climbStairs1(n-2); 
 };
