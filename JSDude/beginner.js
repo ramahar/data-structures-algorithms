6931a9132cf79b8528af4e17d359fd6400cb65be
Rayyan <rmahar@ucsd.edu>
2019-06-24 18:59:56 -0700

    Add problems

--- JSDude/beginner.js
+++ JSDude/beginner.js
@@ -7,3 +7,13 @@ function isPrime(num) {
   return true; 
 }
 // console.log(isPrime(137));
+
+// Find all prime factors of a number 
+function primeFactors(num) {
+  let result = [];
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) result.push(i);
+  }
+  return result;
+}
+console.log(primeFactors(10));
