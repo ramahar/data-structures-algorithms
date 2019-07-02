fefb77754997f9332fdefc3826f85ba6d8b0b697
Rayyan <rmahar@ucsd.edu>
2019-07-01 21:29:57 -0700

    Add problems

--- tech-interview-handbook/array.js
+++ tech-interview-handbook/array.js
@@ -1,1 +1,11 @@
 // Array Practice Questions
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+function twoSum(nums, target) {
+  let map = {};
+    for (let i = 0; i < nums.length; i++) {
+      let diff = target - nums[i];
+      if (map[diff] !== undefined) return [map[diff], i];
+      map[nums[i]] = i;
+    }
+}
+console.log(twoSum([2,7,11,15], 9))
