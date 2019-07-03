97a786b4f48524ff560aa66a4875ae9703135b00
Rayyan <rmahar@ucsd.edu>
2019-07-02 21:25:01 -0700

    Add problems

---
+++ tech-interview-handbook/linked_list.js
@@ -0,0 +1,13 @@
+// Reverse a linked list
+function reverse(head) {
+  let prev = null;
+  while (head) {
+    let next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev; 
+}
+
+// Detect cycle in a linked list
