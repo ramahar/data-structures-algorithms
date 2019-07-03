d48aee581da89cb547ed8be2026bffcf43c5a1ef
Rayyan <rmahar@ucsd.edu>
2019-07-03 01:55:25 -0700

    Add problems

--- tech-interview-handbook/tree.js
+++ tech-interview-handbook/tree.js
@@ -4,3 +4,10 @@ function maxDepth(root) {
 
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 }
+
+// Given two binary trees, write a function to check if they are the same or not.
+function sameTree(p, q) {
+  if (!p || !q) return (p === q);
+
+  return sameTree(p.left, q.right) && sameTree(p.right, q.right) && (p.val === q.val);
+}
