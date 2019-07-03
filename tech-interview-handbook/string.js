2b5d7679c4a3a31b7fc058455800a600097c5a43
Rayyan <rmahar@ucsd.edu>
2019-07-02 22:32:30 -0700

    Add problems

--- tech-interview-handbook/string.js
+++ tech-interview-handbook/string.js
@@ -3,3 +3,23 @@ function isAnagram(s, t) {
   return s.split('').sort().join('') === t.split('').sort().join('');
 }
 // console.log(isAnagram("anagram", "nagaram"));
+
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+// determine if the input string is valid. (Open brackets must be closed by same type of brackets)
+function validParentheses(s) {
+  let pairs = {
+    "(":")",
+    "{":"}",
+    "[":"]"
+  }
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    let el = s[i];
+    if (pairs[el]) stack.push(pairs[el]);
+    else {
+      if (el !== stack.pop()) return false;
+    }
+  }
+  return stack.length === 0;
+}
+// console.log(validParentheses("(){}[]"));
