e1ed77abe1065593d182985fa7a7128c74415abf
Rayyan <rmahar@ucsd.edu>
2019-07-02 21:19:33 -0700

    Add problems

--- 252.meeting-rooms.js
+++ 252.meeting-rooms.js
@@ -2,6 +2,21 @@
 // For example,
 // Given [[0, 30],[5, 10],[15, 20]],
 // return false.
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+**/
 function canAttendMeetings(intervals) {
   // Sort by starting time
+  intervals.sort((a,b) => a.start > b.start ? 1 : -1);
+  for (let i = 1; i < intervals.length; i++) {
+    let prev = intervals[i-1];
+    let curr = intervals[i];
+
+    if (prev.end > curr.start) return false;
+  }
+  return true; 
 }
