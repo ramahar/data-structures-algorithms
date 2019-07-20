7837c5c43a9923450a47ca6ae58a4dce09739509
Rayyan <rmahar@ucsd.edu>
2019-07-20 12:09:20 -0700

    Add new problems

--- tech-interview-handbook/common_algorithms/first_non_repeat.js
+++ tech-interview-handbook/common_algorithms/first_non_repeat.js
@@ -1,3 +1,14 @@
-// Find first non-repeating character in string
+// Find first non-repeating/unique character in string. O(n) ?
+function firstNonRepeat(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (str.indexOf(char) === str.lastIndexOf(char)) return char;
+  }
+  return -1;
+}
+console.log(firstNonRepeat("abacddbec"));
 
-// console.log(firstNonRepeat("abacddbec"));
+// Faster Alternate 
+function firstUnique(str) {
+  
+}
