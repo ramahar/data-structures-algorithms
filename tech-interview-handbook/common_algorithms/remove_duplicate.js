1fede5b96391d0ed8a4a00e8dab561a193dde53d
Rayyan <rmahar@ucsd.edu>
2019-07-20 12:02:57 -0700

    Add new problems

--- tech-interview-handbook/common_algorithms/remove_duplicate.js
+++ tech-interview-handbook/common_algorithms/remove_duplicate.js
@@ -1,6 +1,17 @@
 // Remove duplicates from an array
 function removeDuplicates(arr) {
+  let unique = {};
+  let result = [];
+  arr.forEach(el => {
+    if (!unique[el]) result.push(el);
+    unique[el] = true;
+  });
+  return result;
+}
+console.log(removeDuplicates([1,2,2,2,4,5]));
+
+// Alternative using set 
+function removeDups(arr) {
   let set = new Set(arr);
   return Array.from(set);
 }
-console.log(removeDuplicates([1,2,2,2,4,5]));
