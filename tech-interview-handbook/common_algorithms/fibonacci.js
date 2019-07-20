d6bedefa393ec366ec684c1d5f937a87b7a05884
Rayyan <rmahar@ucsd.edu>
2019-07-20 11:52:56 -0700

    Add new problems

---
+++ tech-interview-handbook/common_algorithms/fibonacci.js
@@ -0,0 +1,17 @@
+// How to get the nth fibonacci number? Recursive O(2^n)
+function fibonacci(n) {
+  if (n <= 1) return n; 
+
+  return fibonacci(n-1) + fibonacci(n-2);
+}
+
+// Memoized fibonacci. Time Complexity: O(n)
+function fib(n) {
+  let memo = {};
+  if (memo[n]) return memo[n];
+
+  if (n <= 1) return n; 
+  memo[n] = fib(n-1) + fib(n-2);
+  return memo[n];
+}
+console.log(fib(12));
