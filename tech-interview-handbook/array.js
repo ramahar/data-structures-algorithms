e24a37df4a604b7e16a6e76b9508215fad638c87
Rayyan <rmahar@ucsd.edu>
2019-07-26 13:24:54 -0700

    Add new problems

--- tech-interview-handbook/array.js
+++ tech-interview-handbook/array.js
@@ -159,13 +159,6 @@ function hasWord(word) {
   
 }
 
-function maxProfit(prices) {
-  let min = prices[0];
-  let max = 0;
-
-  for (let i = 0; i < prices.length; i++) {
-    min = Math.min(min, prices[i]);
-    max = Math.max(max, prices[i] - min);
-  }
-  return max;
+function productExceptSelf(nums) {
+  
 }
