b2b56296d31d6e6976be17953a50d753f56408b8
Rayyan <rmahar@ucsd.edu>
2019-09-16 23:41:40 -0700

    Add problems

--- all_problems.js
+++ all_problems.js
@@ -83,6 +83,89 @@ function maxProduct(arr) {
 function findMin(nums) {
   let min = nums[nums.length-1];  // Compare all elements with last element 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] <= min) return nums[i];
+   if (nums[i] <= min) return nums[i];
+  }
+}
+
+function removeDuplicates(nums) {
+  let count = 0;
+  for (let i = 1; i < nums.length; i++) {
+    let diff = i - count;
+    
+    if (nums[i] === nums[i-1]) count++;   
+    else nums[diff] = nums[i];    // Replace duplicate element in place
+  }
+  return nums.length - count;
+}
+// console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+
+function maxArea(height) {
+  let start = 0, end = height.length - 1;
+  let max = 0;
+
+  while (start < end) {
+    let minHeight = Math.min(height[start], height[end]);
+    let area = minHeight * (end - start);
+    max = Math.max(max, area);
+
+    if (height[start] > height[end]) end--;   // Narrow down container right -> left 
+    else start++;
+  }
+  return max;
+}
+
+// STRING 
+function validAnagram(s, t) {
+  return s.split('').sort().join('') === t.split('').sort().join('');
+}
+
+function isPalindrome(s) {
+  let formatted = s.replace(/\W/g, '');
+  let reversed = s.split('').reverse().join('');
+  return formatted.toLowerCase() === reversed.toLowerCase();
+}
+
+function validParentheses(s) {
+  let pairs = {
+
+  }
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    let el = s[i];
+    if (pairs[el]) stack.push(pairs[el]);
+    else {
+      if (el !== stack.pop()) return false;
+    }
+  }
+  return stack.length === 1;
+}
+
+function longestSubstring(s) {
+  let max = 0;
+  let result = [];
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    let index = result.indexOf(char);
+    result = result.slice(index + 1);
+    max = Math.max(max, result.push(char));
+  }
+  return max; 
+}
+
+function palindromicSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    helper(s, i, i);
+    helper(s, i, i+1);
+  }
+  return count;
+
+  function helper(s, low, high) {
+    // Bound check and palindrome check
+    while (low >= 0 && high <= s.length && s[low] === s[high]) {
+      count++;
+      low--;
+      high++;
+    }
   }
 }
