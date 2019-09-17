47f5f7ce1a53c4d8dd3745c2df2346626e58fd0e
Rayyan <rmahar@ucsd.edu>
2019-09-16 22:01:46 -0700

    Add problems

---
+++ all_problems.js
@@ -0,0 +1,64 @@
+function twoSum(arr, target) {
+	let map = {};
+	for (let i = 0; i < arr.length; i++) {
+		let diff = target - arr[i];
+		if (map[diff] !== undefined) return [map[diff], i];
+		map[arr[i]] = i;
+	}
+}
+// console.log(twoSum([1,3,5], 8));
+
+function maxProfit(prices) {
+	let min = prices[0];
+	let max = 0;
+	
+	for (let i = 1; i < prices.length; i++) {
+		min = Math.min(min, prices[i]);
+		
+	}
+	return max;
+}
+// console.log(maxProfit([1,2,4]));
+
+function containsDuplicate(arr) {
+  let set = new Set(arr);
+  return set.size !== arr.length;
+}
+console.log(containsDuplicate([1,2,2]));
+
+function productExceptSelf(arr) {
+	let result = [];
+	let left = 1, right = 1;
+	for (let i = arr.length-1; i >= 0; i--) {
+		result[i] = right;
+		right *= arr[i];
+	}
+	for (let j = 0; j < arr.length; j++) {
+		result[j] *= left;
+		left *= arr[j];	
+	}
+	return result;
+}
+
+function maxSubArray(arr) {
+	for (let i = 1; i < arr.length; i++) {
+		let max = Math.max(arr[i], arr[i] + arr[i-1]);
+		arr[i] = max;
+	}
+	return Math.max(...arr);
+}
+
+function findDuplicate(arr) {
+	let slow = arr[0];
+	let fast = arr[slow];
+	while (slow !== fast) {
+		slow = arr[slow];
+		fast = arr[arr[fast]];
+	}
+	fast = 0;
+	while (slow !== fast) {
+		slow = arr[slow];
+		fast = arr[fast];
+	}
+	return slow;
+}
