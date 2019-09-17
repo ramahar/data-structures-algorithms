ddd33388be4e3d3becf65376106f026d16b57d2b
Rayyan <rmahar@ucsd.edu>
2019-09-17 01:04:13 -0700

    Add problems

--- all_problems.js
+++ all_problems.js
@@ -169,3 +169,71 @@ function palindromicSubstrings(s) {
     }
   }
 }
+
+function groupAnagrams(strs) {
+  let map = {};
+  for (let str of strs) {
+    let key = str.split('').sort().join('');
+
+    if (!map[str]) map[str] = [];
+    map[key].push(str);
+  }
+  return Object.values(map);
+} 
+
+// LINKED LIST
+function reverse(head) {
+  let prev = null;
+  while (head) {
+    let next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
+
+function hasCycle(head) {
+  let set = new Set();
+  while (head) {
+    if (set.has(head)) return true;
+    set.add(head);
+    head = head.next;
+  }
+  return false;
+}
+
+function removeNth(head) {
+  let curr = head, slow = head, fast = head;
+
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+  if (!fast) return curr.next;
+
+  while (fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  slow.next = slow.next.next;
+  return curr; 
+}
+
+function middle(head) {
+  let slow = head, fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow; 
+}
+
+function merge(l1, l2) {
+  if (!l1 || !l2) return l1 || l2;
+
+  if (l1.val > l2.val) [l1, l2] = [l2, l1];
+  l1.next = merge(l1.next, l2);
+  return l1;
+}
+
+// DYNAMIC PROGRAMMING 
