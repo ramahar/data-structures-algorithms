58a50111b1a6d08245febb5da98c41d6f5f7edaa
Rayyan <rmahar@ucsd.edu>
2019-09-29 17:31:10 -0700

    Add problems

--- all_problems.js
+++ all_problems.js
@@ -1,4 +1,88 @@
-// SORT
+// BINARY SEARCH TREE
+function maxDepth(root) {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
+
+function sameTree(p, q) {
+  if (!p || !q) return p === q;
+  return sameTree(p.left, q.left) && sameTree(p.right, q.right) && (p.val === q.val);
+}
+
+
+
+function levelOrder(root) {
+  let result = [];
+  let queue = [root];
+  while (queue.length > 0) {
+    let inner = [];
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let node = queue.shift();
+      inner.push(node.val);
+
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(inner);
+  }
+  return result;
+}
+
+function inOrder(root) {
+  let stack = [];
+  let result = [];
+  while (stack.length > 0 || root) {
+    if (root) {
+      stack.push(root);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      result.push(root.val);
+      root = root.right;
+    }
+  }
+  return result;
+}
+// root -> left -> right
+function preOrder(root) {
+  if (!root) return [];
+  let stack = [root];
+  let result = [];
+  while (stack.length) {
+    let node = stack.pop();
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return result;
+}
+
+// IMPORTANT ALGORITHMS
+function bfs(root, value) {
+  let queue = [root];
+  while (queue.length > 0) {
+    let node = queue.shift();
+    if (node.value === value) return node;
+
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+  return null;
+}
+
+function dfs(root, value) {
+  let stack = [root];
+  while (stack.length > 0) {
+    let node = stack.pop();
+    if (node.value === value) return node;
+
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
+  }
+  return null;
+}
+
 function mergeSort(arr) {
   if (arr.length <= 1) return arr;
   let mid = Math.floor((arr.length)/2);
