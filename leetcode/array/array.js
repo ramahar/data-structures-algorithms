52b7295cde428ad3b6fb471f902bfa4a1b6ab634
Rayyan <rmahar@ucsd.edu>
2019-10-29 14:47:48 -0700

    Add problems

--- leetcode/array/array.js
+++ leetcode/array/array.js
@@ -7,3 +7,41 @@ function twoSum(arr, target) {
   }
 }
 // console.log(twoSum([1,2,4], 6));
+
+function maxProfit(prices) {
+  let min = prices[0];
+  let max = 0;
+  for (let i = 1; i < prices.length; i++) {
+    min = Math.min(min, prices[i]);
+    max = Math.max(max, prices[i] - min);
+  }
+  return max;
+}
+// console.log(maxProfit([2,1,6,8]));
+
+function containsDuplicate(arr) {
+  let set = new Set(arr);
+  return set.size !== arr.length;
+}
+// console.log(containsDuplicate([1,2,3,3]));
+
+function productExceptSelf(arr) {
+  let result = [];
+  let left = 1;
+  let right = 1;
+  for (let i = arr.length-1; i >= 0; i--) {
+    result[i] = right;
+    right *= arr[i];
+  }
+  for (let j = 0; j < arr.length; j++) {
+    result[j] *= left;
+    left *= arr[j];
+  }
+  return result;
+}
+// console.log(productExceptSelf([1,2,3,5]));
+
+// Return length of maximum subarray 
+function maxSubarray(arr) {
+
+}
