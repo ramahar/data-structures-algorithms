1233b2419c154812ad49757ee069ba0bbed04c5c
Rayyan <rmahar@ucsd.edu>
2019-11-10 16:52:10 -0800

    Add problems

--- leetcode/facebook/facebook.js
+++ leetcode/facebook/facebook.js
@@ -77,3 +77,17 @@ function merge(nums1, m, nums2, n) {
   }
   return nums1;
 }
+
+// Find minimum in rotated sorted array
+//? Idea: Use middle element to decide which half is sorted; then binary search 
+function findMin(nums) {
+  let start = 0, end = nums.length-1;
+  while (start < end) {
+    if (nums[start] < nums[end]) return nums[start];  // No rotation
+    let mid = Math.floor((start+end)/2);
+    if (nums[mid] >= nums[start]) start = mid + 1;   
+    else end = mid; 
+  }
+  return nums[start];
+}
+// console.log(findMin([3,4,5,1,2]));
