819a6e15a4d9c546dc086b06128f6adc7ed2f602
Rayyan <rmahar@ucsd.edu>
2019-11-13 12:38:51 -0800

    Add problems

--- leetcode/string.js
+++ leetcode/string.js
@@ -7,50 +7,85 @@ function validPalindrome(str) {
 function isAnagram(s, t) {
   if (s.length !== t.length) return false;
   let map = {};
-  
+  for (let char of s) {
+    map[char] = (map[char] || 0) + 1;
+  }
+  for (let char of t) {
+    if (!map[char]) return false;
+    map[char]--;    // Decrement char count after compared 
+  }
+  return true;
 }
 
 function validParentheses(str) {
-
+  let pairs = {
+    "(":")",
+    "{":"}",
+    "[":"]"
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let el = str[i];
+    if (pairs[el]) stack.push(pairs[el]);
+    else {
+      if (el !== stack.pop()) return false;
+    }
+  }
+  return stack.length === 0;
 }
 
 function generateParentheses(n) {
+  let result = [];
+  generate(n, n, '');
+  return result;
 
+  function generate(left, right, str) {
+    if (!left && !right && str.length) result.push(str);
+
+    if (left) generate(left - 1, right, str + '(');
+    if (right > left) generate(left, right - 1, str + ')');
+  }
 }
 
+// Given a string, find the length of the longest substring without repeating characters.
 function longestSubstring(str) {
-
-}
-
-function palindromicSubstrings(s) {
-
+  let max = 0;
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    let index = result.indexOf(char);
+    result = result.slice(index + 1);
+    max = Math.max(max, result.push(char));
+  }
+  return max;
 }
 
 function groupAnagrams(strs) {
+  let map = {};
+  for (let str of strs) {
+    let key = str.split('').sort().join('');
 
+    if (!map[key]) map[key] = [];
+    map[key].push(str);
+  }
+  return Object.values(map);
 }
 
-function permutations(str) {
-
-}
-
-
-// Find the longest palindromic substring in s
-function longestPalindrome(s) {
-  let max = '';
+// Count how many palindromic substrings are in the string 
+function palindromicSubstrings(s) {
+  //? Recurse for single digit length palindromes + even digit lengths with lower, upper bounds (index)
+  let count = 0;
   for (let i = 0; i < s.length; i++) {
-    // Handles different palindromes like 'aba' and 'abba'
-    for (let j = 0; j < 2; j++) {
-      let left = i;
-      let right = i + j; 
-      while (s[left] && s[left] === s[right]) {
-        left--;
-        right++;
-      }
-      if ((right - left - 1) > max.length) {
-        max = s.substring(left + 1, right);
-      } 
+    helper(s, i, i);
+    helper(s, i, i + 1);
+  }
+  return count;
+
+  function helper(s, low, high) {
+    while (low >= 0 || high <= s.length || s[low] === s[high]) {
+      count++;
+      low--;
+      high++;
     }
   }
-  return max;
 }
